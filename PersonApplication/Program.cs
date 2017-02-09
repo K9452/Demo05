d9ba55cb730df95b210195a37cae26071963799d
K9452 <K9452@student.jamk.fi>
2017-02-09 15:19:39 +0200

    Demo05 teht 1

--- PersonApplication/Program.cs
+++ PersonApplication/Program.cs
@@ -10,6 +10,48 @@ namespace PersonApplication
     {
         static void Main(string[] args)
         {
+            //creat a few Person objects
+            Person person1 = new Person
+            {
+                FirstName = "Kirsi", LastName ="Mainio", SocialSecurityNumber = "123456789-1234"
+            };
+            Person person2 = new Person { FirstName = "Varus", LastName = "Mies", SocialSecurityNumber = "999999999-666A" };
+            Person person3 = new Person { FirstName = "Jukka", LastName = "Sukkula", SocialSecurityNumber = "021278-199A" };
+
+            // create a Persons object
+            Persons myFriends = new Persons();
+
+            //add person objects to persons collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // get one person from Persons collection
+            Person person4 = myFriends.GetPerson(100);
+            if (person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            } else
+            {
+                Console.WriteLine("Person not found :'(");
+            }
+
+            // print collection
+            myFriends.PrintCollection();
+
+            //find person
+            string sotu = "999999999-666A";
+            Console.WriteLine("Find sotu : " + sotu);
+            Person person5 = myFriends.FindPerson(sotu);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Cant find that person :((");
+            }
+
         }
     }
 }
